2549342244567b0907d420bda8d7a1a06437824a
Jack Herby <JacekZiolek@proton.me>
2025-02-20 22:58:59 +0100

    Add Nuxt UI and readable Tailwind plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,29 @@
 import antfu from '@antfu/eslint-config';
+import eslintPluginReadableTailwind from 'eslint-plugin-readable-tailwind';
+import eslintParserVue from 'vue-eslint-parser';
 
-export default antfu({
-  type: 'app',
-  stylistic: {
-    semi: true,
+export default antfu(
+  {
+    type: 'app',
+    stylistic: {
+      semi: true,
+    },
   },
-});
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parser: eslintParserVue,
+    },
+  },
+  {
+    plugins: {
+      'readable-tailwind': eslintPluginReadableTailwind,
+    },
+    rules: {
+      ...eslintPluginReadableTailwind.configs.warning.rules,
+      ...eslintPluginReadableTailwind.configs.error.rules,
+
+      'node/prefer-global/process': 'off',
+    },
+  },
+);
