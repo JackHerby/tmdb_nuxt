bfb453af3a6980c00017e75061c56bf9660e204b
Jack Herby <JacekZiolek@proton.me>
2025-02-24 21:21:52 +0100

    Add hello api route for testing purposes

--- app.vue
+++ app.vue
@@ -1,3 +1,19 @@
+<script setup lang="ts">
+const { data: hello } = await useFetch('/api/hello');
+
+interface IHello {
+  hello: string;
+}
+
+function isHello(hello: unknown): hello is IHello {
+  return (hello as IHello).hello !== undefined;
+}
+
+if (isHello(hello.value)) {
+  console.log(hello.value.hello);
+}
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
